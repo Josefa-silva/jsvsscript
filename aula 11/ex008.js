db5d15718f099f17278f65084596c16021c16b7b
Josefa-silva <jo.josefa.2016@hotmail.com>
2025-03-18 02:00:06 -0300

    Atualizando meu estudo em JS

---
+++ aula 11/ex008.js
@@ -0,0 +1,12 @@
+console.log("O console funcionou corretamente, Eita glória.");
+
+var vel = 80;
+
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+
+if (vel > 60) {
+  //condição simples
+  console.log(` você oltrapassou a velocidade permitida. MULTADO!`);
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`);
